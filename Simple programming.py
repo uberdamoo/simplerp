b497ef248ddca864f3cfa7e0145d941780814f11
uberdamoo <bardicdm@gmail.com>
2015-08-26 20:37:42 -0400

    Added some functional tests
    Asked for input, checked input, wanted to pass input to function

--- Simple programming.py
+++ Simple programming.py
@@ -1,3 +1,19 @@
 #Simple RP
 	#A program made to simplfy the roleplaying process
-
+"""
+1:Allow User Input
+user = input('What do you want to say to me?  ')
+print ('You said', User, '.')
+2:Check User Input
+user = input('What do you want to say to me?  ')
+if len(user) > 0:
+    print ('You said', user, '.')
+else:
+    print ('type something next time')
+3:Time to talk to a function
+def user(text):
+    "prints what the user passes to function"
+    print (text)
+    return
+4:Let's ask for input and send it to a file
+"""
