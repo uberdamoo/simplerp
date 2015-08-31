4f96da225a7ab24449c8d588e160281311cac99e
uberdamoo <bardicdm@gmail.com>
2015-08-30 22:43:40 -0400

    added loop for multple prompts, added writing into text file

--- Simple programming.py
+++ Simple programming.py
@@ -15,5 +15,28 @@ def user(text):
     "prints what the user passes to function"
     print (text)
     return
-4:Let's ask for input and send it to a file
+4:Let's have the command line keep asking?
+number = 2
+while (number < 99):
+    user = input('What do you want to say to me?  ')
+    if len(user) > 0:
+        print ('You said', user, '.')
+    else:
+        print ('type something next time')
+5:Let's ask for input and send it to a file
 """
+#creates file and works, but does not write to the file if f is not closed.
+#also time is a tuple? doesn't concatanate?
+import time
+f = open('roleplayhistory.txt', 'a')
+number = 2
+while (number < 3):
+    user = input('What do you want to say to me?  ')
+    if len(user) > 0:
+        print ('You said', user, '.')
+        f.write(str(time.localtime()) + '\n') #needs structering
+        f.write(user + '\n')
+        f.close()
+        number = number +2
+    else:
+        print ('type something next time')
